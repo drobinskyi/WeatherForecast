08ab016585310c46e4a7503906b3f30ec79cb555
Vitalii Drobinskyi <drobinskyi@hotmail.com>
2023-04-04 20:00:03 +0300

    Fixed display of daily forecast

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ let weatherBlockDays = document.querySelector('#weather_days');
 let mainCity = 'lviv';
 
 // Отримання назви міста
-let form = document.forms[0];
+let form = document.querySelector(".select_city");
 let cityName = form.elements.city_name;
 function thisCity(event) {
     event.preventDefault();
@@ -34,13 +34,14 @@ async function loadWeather(city) {
         getWeather(responseResult);
         getForecast(responseResult.list);
     } else {
-        weatherBlock.innerHTML = responseResult.message;
+        weatherBlock.innerHTML = "";
         weatherBlockDays.innerHTML = responseResult.message;
     }
 }
 
 // Відображення поточної погоди
 function getWeather(data) {
+
     const location = data.city.name;
     const temp = Math.round(data.list[0].main.temp);
     const feelsLike = Math.round(data.list[0].main.feels_like);
@@ -63,34 +64,41 @@ function getWeather(data) {
                 <p class="box_text">Humidity</p>
                 <p class="box_details">${humid}&#37;</p>
             </div>
-        </div>`;
+        </div>
+        `
 }
 
 // Відображення прогнозу погоди
 function getForecast(forecast){
+    weatherBlockDays.innerHTML = "";
+
     let daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-    for (let i = 0; i < forecast.length; i++) {
-        let time = new Date(forecast[i].dt_txt);
-        let weatherIcon = forecast[i].weather[0].icon;
-        let weatherStatus = forecast[i].weather[0].main;
-        let temperature = Math.round(forecast[i].main.temp);
-        let humidity = Math.round(forecast[i].main.humidity);
+    console.log(forecast);
+    forecast.forEach(el => {
+        const weatherDay = document.createElement("div");
+        weatherDay.classList.add("weather_day");
+
+        let time = new Date(el.dt_txt);
+        let weatherIcon = el.weather[0].icon;
+        let weatherStatus = el.weather[0].main;
+        let temperature = Math.round(el.main.temp);
+        let humidity = Math.round(el.main.humidity);
         let numberOfDay = time.getDay(time);
         let day = daysOfWeek[numberOfDay];
         let hours = time.getHours();
         let minutes = time.getMinutes();
         minutes=minutes>9?minutes:'0'+minutes;
-        
-        weatherBlockDays.innerHTML += `
-        <div class="weather_day">
+
+        weatherDay.innerHTML = `
             <div class="day_time"><p class="day_name">${day}</p><p class="day_hours"> ${hours}:${minutes}</p></div>
             <div class="day_icon">
                 <img class="day_img" src="http://openweathermap.org/img/wn/${weatherIcon}@2x.png" alt="${weatherStatus}">
             </div>
             <div class="day_temp">${temperature}&deg;</div>
             <div class="day_hum">${humidity}&#37;</div>
-        </div>`;
-    }
+        `
+        weatherBlockDays.appendChild(weatherDay);
+    })
 }
 
 if (weatherBlock) {
